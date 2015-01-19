56135be7f9d656c000592b7bad7b0366c06893f1
Kate Hedgpeth <katehedgpeth@gmail.com>
2015-01-18 20:51:53 -0500

    clicking a segment updates the shot record

--- app/assets/javascripts/practice.js
+++ app/assets/javascripts/practice.js
@@ -24,7 +24,7 @@ $(function(){
 
 
 
-
+	var selectionState = 1	
 
 
 
@@ -74,16 +74,15 @@ $(function(){
 	var offset = (topChordLength - bottomChordLength) / 2;
 
 
-	var buttonWidth = diameter/ 4.56;
-	var buttonHeight = buttonWidth / 2
 	
 	
 	var svg = d3.select("#svg").append("svg")
-		.attr("height", diameter)
-		.attr("width", diameter);
+		.attr("height", diameter + 2)
+		.attr("width", diameter + 2);
+
 
 	var g = svg.append("g").attr("transform", function(){
-		return "translate("+ radius +", "+ radius +")"
+//		return "translate("+ (radius - 5) +", "+ (radius - 5) +")"
 	});
 
 
@@ -91,25 +90,26 @@ $(function(){
 	function drawSegment(radius, thisClass, color1, color2) {
 		var points;
 		if (i == 0) { points = 20 }
-		else if (i == 1 ) { points = 1 }
-		else if (i == 2 ) { points = 18 }
-		else if (i == 3 ) { points = 4 }
-		else if (i == 4 ) { points = 13 }
-		else if (i == 5	) { points = 6  }
-		else if (i == 6	) { points = 10 }
-		else if (i == 7	) { points = 15 }
-		else if (i == 8	) { points = 2  }
-		else if (i == 9	) { points = 17 }
+		else if (i == 1 ) { points = 5 }
+		else if (i == 2 ) { points = 12 }
+		else if (i == 3 ) { points = 9 }
+		else if (i == 4 ) { points = 14 }		
+		else if (i == 5	) { points = 11 }
+		else if (i == 6	) { points = 8 }
+		else if (i == 7	) { points = 16 }
+		else if (i == 8	) { points = 7  }
+		else if (i == 9	) { points = 19 }
 		else if (i == 10) { points = 3  }
-		else if (i == 11) { points = 19 }
-		else if (i == 12) { points = 7  }
-		else if (i == 13) { points = 16 }
-		else if (i == 14) { points = 8  }
-		else if (i == 15) { points = 11 }
-		else if (i == 16) { points = 14 }
-		else if (i == 17) { points = 9  }
-		else if (i == 18) { points = 12 }
-		else if (i == 19) { points = 5  }	
+		else if (i == 11) { points = 17 }
+		else if (i == 12) { points = 2  }
+		else if (i == 13) { points = 15 }
+		else if (i == 14) { points = 10  }
+		else if (i == 15) { points = 6 }		
+		else if (i == 16) { points = 13 }
+		else if (i == 17) { points = 4  }
+		else if (i == 18) { points = 18 }
+		else if (i == 19) { points = 1  }	
+
 		function getSideLength(knownSide, angle) {
 	
 			function sinDeg(num) {
@@ -146,7 +146,7 @@ $(function(){
 			})
 			.attr("class", function(){
 				if (i % 2 == 0) {
-					return thisClass + (i+1) +" "+ color1 + " segment";
+					return thisClass + points +" "+ color1 + " segment";
 				} else { return thisClass + points +" "+ color2 + " segment"; }
 			})
 			
@@ -159,15 +159,32 @@ $(function(){
 		var oddLarge = "black"
 		var evenSmall = "green"
 		var evenLarge = "white"
-		drawSegment(lessRadius, "triple", oddSmall, evenSmall);
+		drawSegment(lessRadius, "double", oddSmall, evenSmall);
 		drawSegment(lessRadius * .93, "single", oddLarge, evenLarge);
-		drawSegment(lessRadius * .5, "double", oddSmall, evenSmall);
+		drawSegment(lessRadius * .5, "triple", oddSmall, evenSmall);
 		drawSegment(lessRadius * .43, "single", oddLarge, evenLarge);
 		
 		i++;
 	}
-	g.append("circle").attr("r", radius * .1).attr("class", "bull single green segment");
-	g.append("circle").attr("r", radius * .05).attr("class", "bull double red segment");
+	g.append("circle").attr("r", radius * .1).attr("class", "single bull green segment");
+	g.append("circle").attr("r", radius * .05).attr("class", "double bull red segment");
+	var shots = svg.append("g").attr("class", "shots")
+
+	var buttonWidth = diameter/ 5;
+	var buttonHeight = buttonWidth / 2.5
+
+	var noHitBtn = svg.append("g").attr("class", "no-hit");
+	
+	noHitBtn.append("rect").attr("height", buttonHeight).attr("width", buttonWidth).attr("rx", 5).attr("transform", function(){
+		return "translate("+ (diameter - buttonWidth ) +", "+ (diameter - buttonHeight) +")"
+	});
+	noHitBtn.append("text").attr("transform", function(){
+		return "translate("+ (diameter - (buttonWidth*0.68)) +", "+ (diameter - (buttonHeight*0.4)) +")"
+	}).text("No Hit");
+
+
+
+	g.attr("transform", "rotate(180) translate("+ (-radius - 1) +","+ (-radius -1) +")")
 
 
 	var hoverCircle = svg.append("circle").attr("r", 5).attr("id", "hoverCircle").attr("class", "hidden")
@@ -184,34 +201,37 @@ $(function(){
 		})
 	});
 	$("svg").mouseleave(function(e){
-		console.log("mouseleave");
 		hoverCircle.attr("class", "hidden")
 	});
 	
+	var shotCounter = 0;
+	
 	$(".segment").click(function(e){
-		console.log(this);
-		console.log("clicked " + this.className.baseVal);
+		shotCounter++;
 		tempX = e.pageX; 
 		tempY = e.pageY;
 		offset = $("svg").offset();
 		offsetX = tempX - offset.left - 5;
 		offsetY = tempY - offset.top - 5;
-		d3.select("svg").append("circle").attr("r", 5).attr("class", "shot-marker").attr("transform", function(){
+		shots.append("circle").attr("r", 5).attr("class", "shot-marker").attr("transform", function(){
 			return "translate("+ offsetX +","+ offsetY +")";			
 		});
+		var thisClass = this.className.baseVal;
+		thisSeg = thisClass.substring(0,1);
+		thisNum = thisClass.substring(6,8);
+		if (thisNum == " b"){
+			thisNum = thisClass.substring(7,8);
+		}
+		
+		console.log(thisSeg);
+		console.log(thisNum);
+		$(".shot"+ shotCounter +" h1").html(function(){
+			return thisSeg + thisNum;
+		});
+		
 	});		
 	
-	g.attr("transform", "rotate(180) translate("+ -radius +","+ -radius +")")
 
-
-	var noHitBtn = svg.append("g").attr("class", "no-hit");
-	
-	noHitBtn.append("rect").attr("height", buttonHeight).attr("width", buttonWidth).attr("rx", 5).attr("transform", function(){
-		return "translate("+ (diameter + buttonWidth) +", "+ (diameter - buttonHeight) +")"
-	});
-	noHitBtn.append("text").attr("transform", function(){
-		return "translate("+ (diameter + (buttonWidth*1.5)) +", "+ (diameter - 20) +")"
-	});
 	
 	    
 
